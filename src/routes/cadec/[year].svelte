2012d92c32cf6a4a01199b456c3a269f091f8865
Pär-Anton Westbom <par-antonwestbom@callistaenterprise.se>
2022-01-10 10:59:52 +0100

    cleaned out some unused code

---
+++ src/routes/cadec/[year].svelte
@@ -0,0 +1,12 @@
+<script context="module">
+  export async function load({ page }) {
+    console.log('run load');
+		const year = page.params.year;
+		return { props: { year } };
+	}
+</script>
+<script>
+  export let year;
+</script>
+
+<h1>{year}</h1>
